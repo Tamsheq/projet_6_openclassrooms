49194902d834fa1d8d7f0e19db3caf4e46a6f000
Raphael Abdiche <abdiche.raphael@gmail.com>
2021-11-11 01:35:17 +0100

    projet 6 ok

---
+++ P6_openclassrooms-main/backend/controllers/sauce.js
@@ -0,0 +1,128 @@
+const fs = require('fs');
+const sauce = require('../models/sauce');
+
+// Get all sauces in db
+exports.getAllSauces = (req, res, next) => {
+    sauce.find()
+        .then(newSauce => res.status(200).json(newSauce))
+        .catch(error => res.status(400).json({ error }));
+};
+
+// Get an specific sauce (id params)
+exports.getOneSauce = (req, res, next) => {
+    sauce.findOne({ _id: req.params.id })
+        .then(newSauce => res.status(200).json(newSauce))
+        .catch(error => res.status(404).json({ error }));
+};
+
+// Create new sauce
+exports.createSauce = (req, res, next) => {
+    const sauceObject = JSON.parse(req.body.sauce);
+    const newSauce = new sauce({
+        ...sauceObject,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+        likes: 0,
+        dislikes: 0,
+        usersLiked: '',
+        usersDisliked: ''
+    });
+    newSauce.save()
+        .then(() => res.status(201).json({ message: 'Nouvelle sauce insérée avec succès !' }))
+        .catch(error => res.status(400).json({ error }));
+};
+
+// Update sauce existing
+exports.updateSauce = (req, res, next) => {
+    if (req.file) { // Delete last registered if user load a new img
+        sauce.findOne({ _id: req.params.id })
+            .then(newSauce => {
+                const last_filename = newSauce.imageUrl.split('/images/')[1];
+                fs.unlink('images/' + last_filename, () => { });
+            })
+            .catch(error => console.log('Echec de la suppression de l\'ancienne image.'));
+    }
+    setTimeout(() => {
+        const sauceObject = req.file ? {
+            ...JSON.parse(req.body.sauce),
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        } : { ...req.body };
+        sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+            .catch(error => res.status(400).json({ error }));
+    }, 250);
+};
+
+// Delete sauce
+exports.deleteSauce = (req, res, next) => {
+    sauce.findOne({ _id: req.params.id })
+        .then(newSauce => {
+            const filename = newSauce.imageUrl.split('/images/')[1];
+            fs.unlink('images/' + filename, () => {
+                sauce.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+                    .catch(error => res.status(400).json({ error }));
+            });
+        })
+        .catch(error => res.status(500).json({ error }));
+};
+
+// (dis)like sauce
+exports.likeSauce = (req, res, next) => {
+    sauce.findOne({ _id: req.params.id })
+        .then(newSauce => {
+            let i = 0;
+            let tabLikes = []; let tabDislikes = [];
+            let already_liked = 0; let already_disliked = 0;
+            let type_like = req.body.like;
+            let user_id = req.body.userId;
+
+            // Check if user already disliked this
+            if (newSauce.usersLiked) {
+                tabLikes = JSON.parse(newSauce.usersLiked);
+                while (i < tabLikes.length) {
+                    if (tabLikes[i] == user_id) {
+                        if (type_like == 0 || type_like == -1) { // Delete userid into 'usersLikes' if cancel/dislike
+                            tabLikes.splice(i, 1);
+                            newSauce.likes--;
+                        }
+                        already_liked = 1;
+                    }
+                    i++;
+                }
+            }
+            if (newSauce.usersDisliked) {
+                tabDislikes = JSON.parse(newSauce.usersDisliked); i = 0;
+                while (i < tabDislikes.length) {
+                    if (tabDislikes[i] == user_id) {
+                        if (type_like == 0 || type_like == 1) { // Delete userid into 'usersDislikes' if cancel/like
+                            tabDislikes.splice(i, 1);
+                            newSauce.dislikes--;
+                        }
+                        already_disliked = 1;
+                    }
+                    i++;
+                }
+            }
+
+            // Add dislike
+            if (type_like == 1 && already_liked == 0) {
+                tabLikes.push(user_id);
+                newSauce.likes++;
+            }
+            if (type_like == -1 && already_disliked == 0) {
+                tabDislikes.push(user_id);
+                newSauce.dislikes++;
+            }
+
+            sauce.updateOne({ _id: req.params.id }, {
+                likes: newSauce.likes,
+                dislikes: newSauce.dislikes,
+                usersLiked: JSON.stringify(tabLikes),
+                usersDisliked: JSON.stringify(tabDislikes)
+            })
+                .then(() => res.status(200).json({ message: 'Updated ! ' }))
+                .catch(error => res.status(400).json({ error }));
+
+        })
+        .catch(error => res.status(500).json({ error }));
+};
